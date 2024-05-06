94b02a2f5a571fb70618f50ff374cdbc0738f8a0
Sterdam <novkos2306@gmail.com>
2024-05-06 11:48:37 +0300

    Commented to do things in sql in future

--- sqlrequests.py
+++ sqlrequests.py
@@ -5,16 +5,22 @@ import sqlite3 as sl
 
 class DataBase:
     def __init__(self):
+        # Надо ли подключание в самом классе или не надо?
         self.db = sl.connect('db.sqlite3')
         
+        # user_email надо расширить до нескольких email'ов(приоритет 1)
+        # Надо добавить ban list отправителей, чтобы не приходили емэйлы от каких-то людей,
+        # или конкретно письма от конкретного получателя уведомлялись
         with self.db as con:
             some_sql = con.execute("""CREATE TABLE IF NOT EXISTS user_table 
-                                   (id INTEGER PRIMARY KEY, user_tg INTEGER, user_email TEXT) """)
+                                   (id INTEGER PRIMARY KEY, 
+                                   user_tg INTEGER, 
+                                   user_email TEXT) """)
             
-    def if_table(self):
+    def get_table_names(self):
         with self.db as con:
             some_sql = con.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
         return some_sql.fetchall()
     
 db = DataBase()
-print(db.if_table())
+print(db.get_table_names())
