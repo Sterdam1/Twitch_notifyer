aad549dc84d75799e1975a20ae391cf21a1c540e
Sterdam <novkos2306@gmail.com>
2024-05-12 19:18:08 +0300

    need to change main and hander

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from handlers import router
 
 from sqlrequests import create_tables
 
+from twitch import get_stream_info, is_stream_recently_started
+
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
 
 async def main():
@@ -18,6 +20,18 @@ async def main():
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    
+    # while True:
+    #     streamer_name, stream_start_time = await get_stream_info('buster')
+    #     if stream_start_time:
+    #         if await is_stream_recently_started(stream_start_time):
+    #             message = f"{streamer_name} начал(а) стрим недавно."
+    #         else:
+    #             message = f"{streamer_name} транслирует больше 5 минут."
+    #     else:
+    #         message = f"{streamer_name} оффлайн."
+        
+    #     await asyncio.sleep(300)  # Подождать 5 минут перед следующей проверкой
 
 
 if __name__ == "__main__":
--- twitch.py
+++ twitch.py
@@ -1,50 +1,47 @@
-import requests
-import time
+import aiohttp
+import asyncio
 import datetime
 import pytz
 
+# client_id = "x8oc2925t0s7f4mbu5hqyri18so9ld"  # Вставьте ваш Client ID, полученный при регистрации вашего приложения на Twitch
+# streamer_username = "buster"  # Имя стримера, чью трансляцию вы хотите отслеживать
+# oauth_token = 'k51e937llodslz9gf7o4nc0iu1affe'
 
-client_id = "x8oc2925t0s7f4mbu5hqyri18so9ld"  # Вставьте ваш Client ID, полученный при регистрации вашего приложения на Twitch
-streamer_username = "follentass"  # Имя стримера, чью трансляцию вы хотите отслеживать
-oauth_token = 'k51e937llodslz9gf7o4nc0iu1affe'
-
-
-def check_stream_status():
-    url = f"https://api.twitch.tv/helix/streams?user_login={streamer_username}"
+async def get_stream_info(streamer_name, client_id, oauth_token):
+    url = f"https://api.twitch.tv/helix/streams?user_login={streamer_name}"
     headers = {
         "Client-ID": client_id,
         "Authorization": f"Bearer {oauth_token}"
     }
 
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        if data["data"]:
-            return data["data"][0]['started_at']
-    except Exception as e:
-        print("Error occurred:", e)
-        return False, None
-
-def is_stream_recently_started(start_time):
-    if start_time:
-        stream_start_time = datetime.datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")  # Парсинг времени в формат datetime
-        current_time = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)  # Текущее время с информацией о смещении
-        stream_start_time = stream_start_time.astimezone(pytz.utc)  # Привести время начала стрима к UTC
-        time_difference = current_time - stream_start_time
-        return time_difference.total_seconds() <= 300  # 300 секунд = 5 минут
-
-def main():
-    while True:
-        stream_start_time = check_stream_status()
-        if stream_start_time:
-            if is_stream_recently_started(stream_start_time):
-                print(f"{streamer_username} начал(а) стрим недавно.")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            data = await response.json()
+            if data["data"]:
+                return streamer_name, data["data"][0]["started_at"]
             else:
-                print(f"{streamer_username} не транслирует.")
-        else:
-            print(f"{streamer_username} оффлайн.")
-        
-        time.sleep(60)   # Проверяем статус стрима каждую минуту
+                return None
 
-if __name__ == "__main__":
-    main()
+async def is_stream_recently_started(start_time):
+    if start_time:
+        stream_start_time = datetime.datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.utc)
+        current_time = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+        time_difference = current_time - stream_start_time
+        return time_difference.total_seconds() <= 300 
+    else:
+        return False
+
+async def main():
+    while True:
+        streamer_name, stream_start_time = await get_stream_info('buster')
+        if stream_start_time:
+            if await is_stream_recently_started(stream_start_time):
+                message = f"{streamer_name} начал(а) стрим недавно."
+            else:
+                message = f"{streamer_name} транслирует больше 5 минут."
+        else:
+            message = f"{streamer_name} оффлайн."
+        
+        print(message)
+        
+        await asyncio.sleep(300)  # Подождать 5 минут перед следующей проверкой
