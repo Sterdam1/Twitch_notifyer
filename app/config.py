487f88f814274452d20477d19c425d5f50c2a2dc
Sterdam <novkos2306@gmail.com>
2024-06-10 11:42:49 +0300

    file structure changed

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import json
+
+with open(r'config.json') as file:
+    data = json.load(file)
+
+BOT_TOKEN = data['tg_token']
+TWICH_CLIENT_ID = data['twitch']['client_id']
+TWITCH_OAUTH_TOKEN = data['twitch']['oauth_token']
+
